47f871ba71a516dabab32c1681fff07e1e786dd5
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2021-07-07 22:58:25 -0400

    Adding intro to R slides

---
+++ lessons/02_introR/sandbox/sunspot/sunspots.r
@@ -0,0 +1,48 @@
+# date: 7 July 2021
+
+##### Sunspot data #####
+
+library(tidyverse)
+
+# load the data
+sunData <- read.table(file.choose(), header = TRUE, sep = ",")
+
+# See what the data looks like
+View(sunData)
+
+# Load a data file directly from a path:
+#sunData <- read.table("path/sunSpots.csv", header = TRUE, sep = ",")
+names(sunData)
+
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum))
+
+# Add a smooth line to see general trends
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum))
+
+# Color by year
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum))
+
+# Color by month
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum, color = month)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear))
+
+####
+# Save the file
+# what is the difference between the two graphs below?
+
+png("pngSave_sunSpots_yearVersusSunspots_1.png")
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum))
+dev.off()
+
+ggsave("ggsave_sunSpots_yearVersusSunspots_1.png")
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum, color = month)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear))
+dev.off()
+
+
+# Save the other file
+png("pngSave_sunSpots_yearVersusSunspots_2.png")
+ggplot(data = sunData) +  geom_point(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear)) + geom_smooth(mapping = aes(x = fracOfYear, y = sunspotNum, color = fracOfYear))
+dev.off()
+
+
+# Another way to save the file.
+ggsave("ggsave_sunSpots_yearVersusSunspots_2_i.png")
